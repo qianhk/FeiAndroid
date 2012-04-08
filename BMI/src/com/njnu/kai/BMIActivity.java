7bed38f842be5a913d2ef19692ca5ad7516875ab
feiqhk <flying86@foxmail.com>
2012-04-08 23:17:31 +0800

    add menu

--- BMI/src/com/njnu/kai/BMIActivity.java
+++ BMI/src/com/njnu/kai/BMIActivity.java
@@ -3,12 +3,19 @@ package com.njnu.kai;
 import java.text.DecimalFormat;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BMIActivity extends Activity {
     /** Called when the activity is first created. */
@@ -21,11 +28,12 @@ public class BMIActivity extends Activity {
         button.setOnClickListener(calcBMI);
     }
 
-    private OnClickListener calcBMI = new OnClickListener()
+    private Button.OnClickListener calcBMI = new OnClickListener()
     {
 		@Override
 		public void onClick(View v) {
 			DecimalFormat nf = new DecimalFormat("0.00");
+			try{
 			EditText fieldHeight = (EditText)findViewById(R.id.stature);
 			EditText fieldWeight = (EditText)findViewById(R.id.weight);
 			double height = Double.parseDouble(fieldHeight.getText().toString()) / 100;
@@ -43,6 +51,52 @@ public class BMIActivity extends Activity {
 			else{
 				fieldSuggest.setText(R.string.advice_average);
 			}
+			}
+			catch(Exception err)
+			{
+				Toast.makeText(BMIActivity.this, "error", Toast.LENGTH_SHORT).show();
+			}
+//			openOptionDialog();
 		}
     };
+
+    private void openOptionDialog(){
+//    	Toast.makeText(BMIActivity.this, "haha", Toast.LENGTH_SHORT).show();
+    	new AlertDialog.Builder(BMIActivity.this).setTitle(R.string.about_title).setMessage(R.string.about_msg)
+    	.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+//				Uri uri = Uri.parse("http://wap.google.com");
+//				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//				startActivity(intent);
+			}
+		})
+    	.show();
+    }
+
+    protected static final int MENU_ABOUT = Menu.FIRST;
+    protected static final int MENU_Quit = Menu.FIRST + 1;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	super.onCreateOptionsMenu(menu);
+    	menu.add(0, MENU_ABOUT, 0, "About...");
+    	menu.add(0, MENU_Quit, 0, "Exit");
+    	return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+    	super.onOptionsItemSelected(item);
+    	switch (item.getItemId()){
+    	case MENU_ABOUT:
+    		openOptionDialog();
+    		break;
+
+    	case MENU_Quit:
+    		finish();
+    		break;
+    	}
+
+    	return true;
+    }
 }
