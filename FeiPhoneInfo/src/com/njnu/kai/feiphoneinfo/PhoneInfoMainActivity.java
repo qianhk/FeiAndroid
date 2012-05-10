3637e3366f183094200be00129592e833d52b674
feiatc <qhkyzf@gmail.com>
2012-05-10 16:12:50 +0800

    add feiphoneinfo android version

---
+++ FeiPhoneInfo/src/com/njnu/kai/feiphoneinfo/PhoneInfoMainActivity.java
@@ -0,0 +1,13 @@
+package com.njnu.kai.feiphoneinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhoneInfoMainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
