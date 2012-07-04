c8856b318e71da8ed81f1e743c8cf11afd230fce
feiqhk <flying86@foxmail.com>
2012-07-05 00:09:32 +0800

    added project DivSignin

---
+++ DivSignin/src/com/njnu/kai/divsignin/qianglou/DivQiangLouActivity.java
@@ -0,0 +1,17 @@
+package com.njnu.kai.divsignin.qianglou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.njnu.kai.divsignin.R;
+
+public class DivQiangLouActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.div_qianglou);
+		
+	}
+
+}
