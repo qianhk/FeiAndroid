fe2b0867bcd7bc16ee09172f657f0146a2506e24
feiqhk <flying86@foxmail.com>
2012-07-22 23:28:12 +0800

    add hideSoftInputFromWindow, add tab_smsContent tab_contact

--- FeiSMS/src/com/njnu/kai/feisms/TabContactActivity.java
+++ FeiSMS/src/com/njnu/kai/feisms/TabContactActivity.java
@@ -1,12 +1,32 @@
 package com.njnu.kai.feisms;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
-public class TabContactActivity extends Activity {
+public class TabContactActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab_contact);
 	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+//		Log.i("TabContact", "onResume");
+//		getListView().requestFocus();
+
+		View currentFocus = getListView();
+		IBinder windowToken = currentFocus != null ? currentFocus.getWindowToken() : null;
+//		Log.i("gaga", " focusView=" + currentFocus + " token=" + windowToken);
+		if (windowToken != null) {
+			((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS);
+		}
+	}
+
 }
--- FeiSMS/src/com/njnu/kai/feisms/TabSMSContentActivity.java
+++ FeiSMS/src/com/njnu/kai/feisms/TabSMSContentActivity.java
@@ -2,6 +2,9 @@ package com.njnu.kai.feisms;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
 
 public class TabSMSContentActivity extends Activity {
 
@@ -9,6 +12,24 @@ public class TabSMSContentActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tab_sms);
+	}
+
+	@Override
+	protected void onPause() {
+//		View currentFocus = getCurrentFocus();
+//		IBinder windowToken = currentFocus != null ? currentFocus.getWindowToken() : null;
+//		Log.i("haha", " focusView=" + currentFocus + " token=" + windowToken);
+		super.onPause();
+//		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//		// 得到InputMethodManager的实例
+//		if (imm.isActive()) {
+//			// 如果开启
+//			Log.i("TabSmsContent", "activie");
+//			imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, InputMethodManager.HIDE_NOT_ALWAYS);
+//			// 关闭软键盘，开启方法相同，这个方法是切换开启与关闭状态的
+//		}
+//		((InputMethodManager) getSystemService(INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+//				InputMethodManager.HIDE_NOT_ALWAYS);
 	}
 
 }
