4a7df1e4f37a46996711e77cd922f222a0137f50
feiatc <qhkyzf@gmail.com>
2012-07-26 17:26:48 +0800

    AppWidgetProvider's field must be static.

--- WidgetTime/src/com/njnu/kai/widget/time/WidgetTimeProvider.java
+++ WidgetTime/src/com/njnu/kai/widget/time/WidgetTimeProvider.java
@@ -17,23 +17,9 @@ public class WidgetTimeProvider extends AppWidgetProvider {
 
 	private static final String FRESH = "com.njnu.kai.widgettime.fresh";
 	private Context mContext;
-	private boolean mRun = false;
-	private Thread myThread = new Thread() {
-
-		@Override
-		public void run() {
-			while (mRun) {
-				try {
-					Thread.sleep(1000);
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
-				Log.v("WidgetTiemProvider", "Thread sleep 1000 " + mRun);
-				mContext.sendBroadcast(new Intent(FRESH));
-			}
-		}
-		
-	};
+	private static boolean mRun = false;
+	private Thread myThread;
+	
 	public static class UpdateService extends Service {
 
 		@Override
@@ -58,7 +44,7 @@ public class WidgetTimeProvider extends AppWidgetProvider {
 	public void onReceive(Context context, Intent intent) {
 		super.onReceive(context, intent);
 		String action = intent.getAction();
-		Log.v("TimeWidget", "onReceive " + action + " mRun=" + mRun);
+//		Log.v("TimeWidget", "onReceive " + action + " mRun=" + mRun);
 		if (action.equals(FRESH)) {			
 			Intent intent2 = new Intent(context, UpdateService.class);
 			context.startService(intent2);
@@ -67,26 +53,42 @@ public class WidgetTimeProvider extends AppWidgetProvider {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		Log.v("TimeProvider", "onUpdate22222");
+		Log.v("TimeProvider", "onUpdate22222 context=" + context);
 		mContext = context;
 		mRun = true;
 		Intent intent = new Intent(context, UpdateService.class);
 		context.startService(intent);
 //		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		myThread = new Thread() {
+
+			@Override
+			public void run() {
+				while (mRun) {
+					try {
+						Thread.sleep(1000);
+					} catch (Exception e) {
+						// TODO: handle exception
+					}
+//					Log.v("WidgetTiemProvider", "Thread sleep 1000 " + mRun);
+					mContext.sendBroadcast(new Intent(FRESH));
+				}
+			}
+			
+		};
 		myThread.start();
 //		context.registerReceiver()
 	}
 
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds) {
-		Log.v("TimeProvider", "onDeleted");
+		Log.v("TimeProvider", "onDeleted " + this + " context=" + mContext);
 		super.onDeleted(context, appWidgetIds);
 		mRun = false;
 	}
 
 	@Override
 	public void onEnabled(Context context) {
-		Log.v("TimeProvider", "onEnabled");
+		Log.v("TimeProvider", "onEnabled " + this + " context=" + mContext);
 		super.onEnabled(context);
 		mRun = true;
 	}
@@ -94,7 +96,7 @@ public class WidgetTimeProvider extends AppWidgetProvider {
 	@Override
 	public void onDisabled(Context context) {
 		super.onDisabled(context);
-		Log.v("TimeProvider", "onDisable");
+		Log.v("TimeProvider", "onDisable " + this + " context=" + mContext);
 		mRun = false;
 	}
 
