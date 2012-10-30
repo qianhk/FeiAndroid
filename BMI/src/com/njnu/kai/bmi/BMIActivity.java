e9982446741ec6f914dd65eca939b5bf3fd00395
feiatc <qhkyzf@gmail.com>
2012-10-30 11:21:55 +0800

    update for test http cmwap log.

--- BMI/src/com/njnu/kai/bmi/BMIActivity.java
+++ BMI/src/com/njnu/kai/bmi/BMIActivity.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,145 +26,183 @@ public class BMIActivity extends Activity implements OnClickListener {
 
 	private ImageView mIvTestLayer1;
 	private ImageView mIvTestLayer2;
+	private View mLayout1;
+	private View mLayout2;
+	private EditText mEdtResult;
+	private StringBuilder mBuilder;
 
 	public double calcBMI(double weight, double height) {
-    	double BMI = weight / (height * height);
-    	return BMI;
-    }
+		double BMI = weight / (height * height);
+		return BMI;
+	}
 
-    private Button.OnClickListener calcBMI = new OnClickListener()
-    {
+	private Button.OnClickListener calcBMI = new OnClickListener()
+	{
 		@Override
 		public void onClick(View v) {
 			DecimalFormat nf = new DecimalFormat("0.00");
-			try{
-			EditText fieldHeight = (EditText)findViewById(R.id.stature);
-			EditText fieldWeight = (EditText)findViewById(R.id.weight);
-			double height = Double.parseDouble(fieldHeight.getText().toString()) / 100;
-			double weight = Double.parseDouble(fieldWeight.getText().toString());
-			double BMI = calcBMI(weight, height);
+			try {
+				EditText fieldHeight = (EditText)findViewById(R.id.stature);
+				EditText fieldWeight = (EditText)findViewById(R.id.weight);
+				double height = Double.parseDouble(fieldHeight.getText().toString()) / 100;
+				double weight = Double.parseDouble(fieldWeight.getText().toString());
+				double BMI = calcBMI(weight, height);
 
-			TextView result = (TextView)findViewById(R.id.result);
-			result.setText("Your BMI is " + nf.format(BMI));
-			TextView fieldSuggest = (TextView)findViewById(R.id.suggest);
-			if (BMI > 25){
-				fieldSuggest.setText(R.string.advice_heavy);
-			} else if(BMI < 20){
-				fieldSuggest.setText(R.string.advice_light);
+				TextView result = (TextView)findViewById(R.id.result);
+				result.setText("Your BMI is " + nf.format(BMI));
+				TextView fieldSuggest = (TextView)findViewById(R.id.suggest);
+				if (BMI > 25) {
+					fieldSuggest.setText(R.string.advice_heavy);
+				} else if (BMI < 20) {
+					fieldSuggest.setText(R.string.advice_light);
+				}
+				else {
+					fieldSuggest.setText(R.string.advice_average);
+				}
 			}
-			else{
-				fieldSuggest.setText(R.string.advice_average);
-			}
-			}
-			catch(Exception err)
+			catch (Exception err)
 			{
 				Toast.makeText(BMIActivity.this, "error", Toast.LENGTH_SHORT).show();
 			}
 //			openOptionDialog();
 		}
-    };
+	};
 
-    private void openOptionDialog(){
+	private void openOptionDialog() {
 //    	Toast.makeText(BMIActivity.this, "haha", Toast.LENGTH_SHORT).show();
-    	new AlertDialog.Builder(BMIActivity.this).setTitle(R.string.about_title).setMessage(R.string.about_msg)
-    	.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
+		new AlertDialog.Builder(BMIActivity.this).setTitle(R.string.about_title).setMessage(R.string.about_msg)
+				.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
 //				Uri uri = Uri.parse("http://wap.google.com");
 //				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 //				startActivity(intent);
-			}
-		})
-    	.show();
-    }
+					}
+				})
+				.show();
+	}
 
-    protected static final int MENU_ABOUT = Menu.FIRST;
-    protected static final int MENU_Quit = Menu.FIRST + 1;
+	protected static final int MENU_ABOUT = Menu.FIRST;
+	protected static final int MENU_Quit = Menu.FIRST + 1;
+	protected static final int MENU_Email = Menu.FIRST + 2;
+	protected static final int MENU_Email_Clear = Menu.FIRST + 3;
 	private Drawable mDrawableLauncher2;
 	private Drawable mDrawableApple;
 	private Drawable mDrawableLauncher1;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-    	super.onCreateOptionsMenu(menu);
-    	menu.add(0, MENU_ABOUT, 0, "About...");
-    	menu.add(0, MENU_Quit, 0, "Exit");
-    	return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, MENU_Email, 0, "Send...");
+		menu.add(0, MENU_Email_Clear, 10, "Clear Send String");
+		menu.add(0, MENU_ABOUT, 20, "About...");
+		menu.add(0, MENU_Quit, 30, "Exit");
+		return true;
+	}
 
-    public boolean onOptionsItemSelected(MenuItem item){
-    	super.onOptionsItemSelected(item);
-    	switch (item.getItemId()){
-    	case MENU_ABOUT:
-    		openOptionDialog();
-    		break;
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+		switch (item.getItemId()) {
+		case MENU_ABOUT:
+			openOptionDialog();
+			break;
 
-    	case MENU_Quit:
-    		finish();
-    		break;
-    	}
+		case MENU_Quit:
+			finish();
+			break;
 
-    	return true;
-    }
+		case MENU_Email:
+			sendLogEmail();
+			break;
 
-    public void clickTestLayer1(View v) {
-    	LayerDrawable d = (LayerDrawable)mIvTestLayer2.getDrawable();
-    	d.setDrawableByLayerId(1, mDrawableLauncher2);
-    	d.invalidateSelf();
-    	mIvTestLayer2.invalidate();
+		case MENU_Email_Clear:
+			mBuilder.setLength(0);
+			break;
 
-    	d = (LayerDrawable)mIvTestLayer1.getDrawable();
-    	d.setDrawableByLayerId(1, mDrawableLauncher1);
-    	d.invalidateSelf();
-    	mIvTestLayer1.invalidate();
+		default:
+			break;
+		}
 
-    	setSettingPanelColorButtonFlag(2);
-    }
+		return true;
+	}
 
-    public void clickTestLayer2(View v) {
+	private void sendLogEmail() {
+		Intent it = new Intent(Intent.ACTION_SEND);
+		String[] toWho = {
+				"\u0068\u006f\u006e\u0067\u006b\u0061\u0069\u002e\u0071\u0069\u0061\u006e\u0040\u0074\u0074\u0070\u006f\u0064\u002e\u0063\u006f\u006d",
+				"\u007a\u0068\u0065\u006e\u0068\u0075\u0061\u002e\u0067\u0061\u006f\u0040\u0074\u0074\u0070\u006f\u0064\u002e\u0063\u006f\u006d"};
+		it.putExtra(Intent.EXTRA_EMAIL, toWho);
+		it.putExtra(Intent.EXTRA_SUBJECT, "tt http test log");
+		it.putExtra(Intent.EXTRA_TEXT, mBuilder.toString());
+		it.setType("text/plain");
+		startActivity(Intent.createChooser(it, "Choose Email Client"));
+	}
+
+	public void clickTestLayer1(View v) {
+		LayerDrawable d = (LayerDrawable)mIvTestLayer2.getDrawable();
+		d.setDrawableByLayerId(1, mDrawableLauncher2);
+		d.invalidateSelf();
+		mIvTestLayer2.invalidate();
+
+		d = (LayerDrawable)mIvTestLayer1.getDrawable();
+		d.setDrawableByLayerId(1, mDrawableLauncher1);
+		d.invalidateSelf();
+		mIvTestLayer1.invalidate();
+
+		setSettingPanelColorButtonFlag(2);
+	}
+
+	public void clickTestLayer2(View v) {
 //    	mIvTestLayer2.setImageDrawable(mIvTestLayer1.getDrawable().mutate());
-    	LayerDrawable d = (LayerDrawable)mIvTestLayer2.getDrawable();
-    	d.setDrawableByLayerId(1, mDrawableLauncher1);
-    	d.invalidateSelf();
-    	mIvTestLayer2.invalidate();
+		LayerDrawable d = (LayerDrawable)mIvTestLayer2.getDrawable();
+		d.setDrawableByLayerId(1, mDrawableLauncher1);
+		d.invalidateSelf();
+		mIvTestLayer2.invalidate();
 
-    	d = (LayerDrawable)mIvTestLayer1.getDrawable();
-    	d.setDrawableByLayerId(1, mDrawableLauncher2);
-    	d.invalidateSelf();
-    	mIvTestLayer1.invalidate();
+		d = (LayerDrawable)mIvTestLayer1.getDrawable();
+		d.setDrawableByLayerId(1, mDrawableLauncher2);
+		d.invalidateSelf();
+		mIvTestLayer1.invalidate();
 
-    	setSettingPanelColorButtonFlag(3);
-    }
+		setSettingPanelColorButtonFlag(3);
+	}
 
 	private View[] mArrayColorButton;
 	private ColorButtonManager mColorButtonMgr;
 	private Drawable mDrawableFlag;
 	private Drawable mDrawableTransparent;
 
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-        Button button = (Button)findViewById(R.id.submit);
-        button.setOnClickListener(calcBMI);
+		Button button = (Button)findViewById(R.id.submit);
+		button.setOnClickListener(calcBMI);
 
-        mIvTestLayer1 = (ImageView)findViewById(R.id.iv_testlayer1);
-        mIvTestLayer2 = (ImageView)findViewById(R.id.iv_testlayer2);
-        mDrawableApple = getResources().getDrawable(R.drawable.minilyric_btn_color_blue);
+		mIvTestLayer1 = (ImageView)findViewById(R.id.iv_testlayer1);
+		mIvTestLayer2 = (ImageView)findViewById(R.id.iv_testlayer2);
+		mDrawableApple = getResources().getDrawable(R.drawable.minilyric_btn_color_blue);
 		mDrawableLauncher2 = getResources().getDrawable(R.drawable.ic_launcher2);
 		mDrawableLauncher1 = getResources().getDrawable(R.drawable.ic_launcher);
-		LayerDrawable layerdrawable = new LayerDrawable(new Drawable[] {mDrawableApple, mDrawableLauncher2});
-        layerdrawable.setId(1, 1);
-        mIvTestLayer1.setImageDrawable(layerdrawable);
+		LayerDrawable layerdrawable = new LayerDrawable(new Drawable[]{mDrawableApple, mDrawableLauncher2});
+		layerdrawable.setId(1, 1);
+		mIvTestLayer1.setImageDrawable(layerdrawable);
 
-        layerdrawable = new LayerDrawable(new Drawable[] {mDrawableApple, mDrawableLauncher2});
-        layerdrawable.setId(1, 1);
+		layerdrawable = new LayerDrawable(new Drawable[]{mDrawableApple, mDrawableLauncher2});
+		layerdrawable.setId(1, 1);
 		mIvTestLayer2.setImageDrawable(layerdrawable);
 
 		initColorButton();
-    }
+
+		mLayout1 = findViewById(R.id.layout_1);
+		mLayout2 = findViewById(R.id.layout_2);
+		mEdtResult = (EditText)findViewById(R.id.edt_result);
+		mLayout2.setVisibility(View.INVISIBLE);
+
+		mBuilder = new StringBuilder(512);
+	}
 
 	private class ColorButtonManager {
 		private ArrayList<ColorButton> mList;
@@ -172,6 +212,7 @@ public class BMIActivity extends Activity implements OnClickListener {
 			private Drawable mImage;
 			private LayerDrawable mDrawable;
 			private int mId;
+
 //			private int mRed[] = {R.drawable.minilyric_icon_setting, R.drawable.minilyric_icon_fontzoomin, R.drawable.minilyric_icon_fontzoomout
 //					, R.drawable.minilyric_return_main_panel_normal, R.drawable.minilyric_unlocked};
 
@@ -179,7 +220,7 @@ public class BMIActivity extends Activity implements OnClickListener {
 				mId = id;
 				mImageButton = (ImageView)button;
 				mImage = image;
-				mDrawable = new LayerDrawable(new Drawable[] {mImage, mDrawableTransparent});
+				mDrawable = new LayerDrawable(new Drawable[]{mImage, mDrawableTransparent});
 				mDrawable.setId(1, 1);
 				mImageButton.setImageDrawable(mDrawable);
 			}
@@ -257,5 +298,44 @@ public class BMIActivity extends Activity implements OnClickListener {
 				break;
 			}
 		}
+		doColorButtonClicked(view);
+	}
+
+	private void doColorButtonClicked(View v) {
+		mLayout1.setVisibility(View.INVISIBLE);
+		mLayout2.setVisibility(View.VISIBLE);
+		String result = "";
+		String from = "";
+		switch (v.getId()) {
+		case R.id.iv_color_blue:
+			from = "blue:\n\n";
+			result = HttpConnectionUtility.GetUseAutoEncoding(this, HttpUtility.TTGETHEADER, true);
+			break;
+
+		case R.id.iv_color_yellow:
+			from = "\n\nyellow:\n\n";
+			result = HttpConnectionUtility.GetUseAutoEncoding(this, HttpUtility.TTLRCSEARCH, true);
+			break;
+
+		case R.id.iv_color_pink:
+			from = "\n\npink:\n\n";
+			result = HttpConnectionUtility.GetUseAutoEncoding(this, HttpUtility.TTLRCDOWN, true);
+			break;
+
+		case R.id.iv_color_gray:
+			from = "\n\ngray:\n\n";
+			result = HttpConnectionUtility.GetUseAutoEncoding(this, HttpUtility.TTGETHEADER, false);
+			break;
+
+		case R.id.iv_color_green:
+			from = "\n\ngreen:\n\n";
+			result = HttpConnectionUtility.GetUseAutoEncoding(this, HttpUtility.TTLRCDOWN, false);
+			break;
+
+		default:
+			break;
+		}
+		mEdtResult.setText(result);
+		mBuilder.append(from + result);
 	}
 }
