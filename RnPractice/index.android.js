7df6ad7b8dd283a5b5d0e36acc67fe193e309416
qianhk <qhk83@qq.com>
2016-05-27 00:09:40 +0800

    update for can test

--- RnPractice/index.android.js
+++ RnPractice/index.android.js
@@ -19,7 +19,7 @@ class RnPractice extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          To get started, edit index.android.js  kai掌
         </Text>
         <Text style={styles.instructions}>
           Shake or press menu button for dev menu
