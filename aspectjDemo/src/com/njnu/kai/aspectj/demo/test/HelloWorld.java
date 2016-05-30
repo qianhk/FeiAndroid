50532f8a72e2aa70383a92f9dfa62bbfc52b61fc
feiatc <qhk83@qq.com>
2016-05-30 17:55:59 +0800

    add target this within different test

--- aspectjDemo/src/com/njnu/kai/aspectj/demo/test/HelloWorld.java
+++ aspectjDemo/src/com/njnu/kai/aspectj/demo/test/HelloWorld.java
@@ -1,15 +1,29 @@
 package com.njnu.kai.aspectj.demo.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.njnu.kai.aspectj.demo.different.Animal;
+import com.njnu.kai.aspectj.demo.different.Bird;
+import com.njnu.kai.aspectj.demo.different.Snake;
+import com.njnu.kai.aspectj.demo.different.TestMove;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("test aspectj demo in main");
-		printI(5);
-		HelloWorld2.main(args);
-
-		HelloWorld hw = new HelloWorld();
-		hw.bar();
-		hw.foo();
+//		System.out.println("test aspectj demo in main");
+//		printI(5);
+//		HelloWorld2.main(args);
+//
+//		HelloWorld hw = new HelloWorld();
+//		hw.bar();
+//		hw.foo();
+		
+		List<Animal> list = new ArrayList<Animal>();  
+        list.add(new Bird());  
+        list.add(new Snake());  
+        TestMove tm = new TestMove();
+        tm.move(list);  
 	}
 
 	public static void printI(int i) {
