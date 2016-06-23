645a1cdad11ce327fd4e17f2516cf070169efdd1
feiatc <qhk83@qq.com>
2016-06-23 11:11:56 +0800

    add 2048 game test rn

--- RnPractice/index.android.js
+++ RnPractice/index.android.js
@@ -15,6 +15,7 @@ const ZhiHuDaily = require('./zhihu/ZhihuDaily');
 import MovieFetcher from './MovieFetcher';
 const MoviesApp = require('./movie/MoviesApp.android');
 const TicTacToeApp = require('./TicTacToeApp');
-const UIExplorerApp = require('./UIExplorer/UIExplorerApp.android')
+const UIExplorerApp = require('./UIExplorer/UIExplorerApp.android');
+const Game2048 = require('./2048/Game2048');
 
-AppRegistry.registerComponent('RnPractice', () => UIExplorerApp);
+AppRegistry.registerComponent('RnPractice', () => Game2048);
