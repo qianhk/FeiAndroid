b76dcec7b73bb62486f7911b8a66b34a1f869a26
feiatc <qhk83@qq.com>
2016-07-28 14:53:55 +0800

    注释测试代码,有问题导致idea提示save setting错误

--- PluginPractice/src/com/njnu/kai/plugin/practice/SayHiAction.java
+++ PluginPractice/src/com/njnu/kai/plugin/practice/SayHiAction.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.ui.Messages;
 
 /**
  * @author hongkai.qian
@@ -14,9 +15,9 @@ public class SayHiAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-        final Application application = ApplicationManager.getApplication();
-        final KaiAppComponent kaiAppComponent = application.getComponent(KaiAppComponent.class);
-        kaiAppComponent.sayHi();
-//        Messages.showMessageDialog("Hello world, Plugin!", "Kai Plugin", Messages.getInformationIcon());
+//        final Application application = ApplicationManager.getApplication();
+//        final KaiAppComponent kaiAppComponent = application.getComponent(KaiAppComponent.class);
+//        kaiAppComponent.sayHi();
+        Messages.showMessageDialog("Hello world, Plugin!", "Kai Plugin", Messages.getInformationIcon());
     }
 }
