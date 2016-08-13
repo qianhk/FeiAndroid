2136b827d968b462ae2a351e564b9bb0f9eae700
qianhk <qhk83@qq.com>
2016-08-13 11:42:30 +0800

    test applicationService, but return null

--- PluginPractice/src/com/njnu/kai/plugin/preference/configurable/template/TemplateConfigurable.java
+++ PluginPractice/src/com/njnu/kai/plugin/preference/configurable/template/TemplateConfigurable.java
@@ -1,10 +1,26 @@
 package com.njnu.kai.plugin.preference.configurable.template;
 
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.highlighter.JavaFileHighlighter;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.editor.EditorSettings;
+import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.editor.event.DocumentAdapter;
+import com.intellij.openapi.editor.event.DocumentEvent;
+import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.editor.ex.util.LexerEditorHighlighter;
 import com.intellij.openapi.options.BaseConfigurable;
 import com.intellij.openapi.options.ConfigurationException;
-import com.njnu.kai.plugin.practice.HelloWorldConfigurationForm;
+import com.intellij.ui.SeparatorFactory;
+import com.intellij.ui.roots.ToolbarPanel;
 import com.njnu.kai.plugin.preference.persistence.TemplateSettings;
+import com.njnu.kai.plugin.util.DialogsFactory;
+import com.njnu.kai.plugin.util.StringResources;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
 
@@ -18,100 +34,99 @@ import java.awt.*;
  */
 public class TemplateConfigurable extends BaseConfigurable {
 
-    private JPanel editorPanel = new JPanel(new GridLayout());
+    private JPanel mEditorPanel = new JPanel(new GridLayout());
+    private Editor mEditor;
 
-    private Editor editor;
+    private TemplateSettings mTemplateSettings;
 
-    private TemplateSettings templateSettings;
-
-    private final String templateName;
-    private final String templateHeaderText;
-    private final String displayName;
+    private final String mTemplateName;
+    private final String mTemplateHeaderText;
+    private final String mDisplayName;
 
     public TemplateConfigurable(String displayName, String templateHeaderText, String templateName) {
-        this.displayName = displayName;
-        this.templateHeaderText = templateHeaderText;
-        this.templateName = templateName;
+        mDisplayName = displayName;
+        mTemplateHeaderText = templateHeaderText;
+        mTemplateName = templateName;
     }
 
     @Nullable
     @Override
     public JComponent createComponent() {
-//        templateSettings = TemplateSettings.getInstance();
-//        editor = createEditorInPanel(templateSettings.provideTemplateForName(templateName));
-//
-//        JPanel panel = new JPanel(new BorderLayout());
-//        panel.setPreferredSize(new Dimension(400, 300));
-//        panel.add(SeparatorFactory.createSeparator(templateHeaderText, null), BorderLayout.PAGE_START);
-//        panel.add(new ToolbarPanel(editorPanel, new DefaultActionGroup(new ResetToDefaultAction())), BorderLayout.CENTER);
-//        return panel;
+        mTemplateSettings = TemplateSettings.getInstance();
+        if (mTemplateSettings == null) {
+            return new JLabel("TemplateSettings.getInstance() return null : " + mTemplateHeaderText);
+        }
+        mEditor = createEditorInPanel(mTemplateSettings.provideTemplateForName(mTemplateName));
 
-        final HelloWorldConfigurationForm helloWorldConfigurationForm = new HelloWorldConfigurationForm();
-        helloWorldConfigurationForm.setTitleText(templateName);
-        helloWorldConfigurationForm.setMsgText(templateHeaderText);
-        return helloWorldConfigurationForm.getRootView();
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setPreferredSize(new Dimension(400, 300));
+        panel.add(SeparatorFactory.createSeparator(mTemplateHeaderText, null), BorderLayout.PAGE_START);
+        panel.add(new ToolbarPanel(mEditorPanel, new DefaultActionGroup(new ResetToDefaultAction())), BorderLayout.CENTER);
+        return panel;
     }
 
-//    private Editor createEditorInPanel(String string) {
-//        EditorFactory editorFactory = EditorFactory.getInstance();
-//        Editor editor = editorFactory.createEditor(editorFactory.createDocument(string));
-//
-//        EditorSettings editorSettings = editor.getSettings();
-//        editorSettings.setVirtualSpace(false);
-//        editorSettings.setLineMarkerAreaShown(false);
-//        editorSettings.setIndentGuidesShown(false);
-//        editorSettings.setLineNumbersShown(false);
-//        editorSettings.setFoldingOutlineShown(false);
-//        editorSettings.setAdditionalColumnsCount(3);
-//        editorSettings.setAdditionalLinesCount(3);
-//
-//        editor.getDocument().addDocumentListener(new DocumentAdapter() {
-//            @Override
-//            public void documentChanged(DocumentEvent e) {
-//                onTextChanged();
-//            }
-//        });
-//
-//        ((EditorEx) editor).setHighlighter(getEditorHighlighter());
-//
-//        addEditorToPanel(editor);
-//
-//        return editor;
-//    }
-//
-//    private LexerEditorHighlighter getEditorHighlighter() {
-//        return new LexerEditorHighlighter(new JavaFileHighlighter(), EditorColorsManager.getInstance().getGlobalScheme());
-//    }
-//
-//    private void onTextChanged() {
-//        myModified = true;
-//    }
-//
-//    private void addEditorToPanel(Editor editor) {
-//        editorPanel.removeAll();
-//        editorPanel.add(editor.getComponent());
-//    }
+    private Editor createEditorInPanel(String string) {
+        EditorFactory editorFactory = EditorFactory.getInstance();
+        Editor editor = editorFactory.createEditor(editorFactory.createDocument(string));
+
+        EditorSettings editorSettings = editor.getSettings();
+        editorSettings.setVirtualSpace(false);
+        editorSettings.setLineMarkerAreaShown(false);
+        editorSettings.setIndentGuidesShown(false);
+        editorSettings.setLineNumbersShown(false);
+        editorSettings.setFoldingOutlineShown(false);
+        editorSettings.setAdditionalColumnsCount(3);
+        editorSettings.setAdditionalLinesCount(3);
+
+        editor.getDocument().addDocumentListener(new DocumentAdapter() {
+            @Override
+            public void documentChanged(DocumentEvent e) {
+                onTextChanged();
+            }
+        });
+
+        ((EditorEx) editor).setHighlighter(getEditorHighlighter());
+
+        addEditorToPanel(editor);
+
+        return editor;
+    }
+
+    private LexerEditorHighlighter getEditorHighlighter() {
+        return new LexerEditorHighlighter(new JavaFileHighlighter(), EditorColorsManager.getInstance().getGlobalScheme());
+    }
+
+    private void onTextChanged() {
+        myModified = true;
+    }
+
+    private void addEditorToPanel(Editor editor) {
+        mEditorPanel.removeAll();
+        mEditorPanel.add(editor.getComponent());
+    }
 
     @Override
     public void disposeUIResources() {
-//        if (editor != null) {
-//            EditorFactory.getInstance().releaseEditor(editor);
-//            editor = null;
-//        }
-//        templateSettings = null;
+        if (mEditor != null) {
+            EditorFactory.getInstance().releaseEditor(mEditor);
+            mEditor = null;
+        }
+        mTemplateSettings = null;
     }
 
     @Override
     public void apply() throws ConfigurationException {
-//        templateSettings.setTemplateForName(templateName, editor.getDocument().getText());
-//        setUnmodified();
+        mTemplateSettings.setTemplateForName(mTemplateName, mEditor.getDocument().getText());
+        setUnmodified();
     }
 
     @Override
     public void reset() {
-//        EditorFactory.getInstance().releaseEditor(editor);
-//        editor = createEditorInPanel(templateSettings.provideTemplateForName(templateName));
-//        setUnmodified();
+        if (mEditor != null) {
+            EditorFactory.getInstance().releaseEditor(mEditor);
+            mEditor = createEditorInPanel(mTemplateSettings.provideTemplateForName(mTemplateName));
+        }
+        setUnmodified();
     }
 
     private void setUnmodified() {
@@ -121,12 +136,12 @@ public class TemplateConfigurable extends BaseConfigurable {
     @Nls
     @Override
     public String getDisplayName() {
-        return displayName;
+        return mDisplayName;
     }
 
     @Override
     public JComponent getPreferredFocusedComponent() {
-        return editorPanel;
+        return mEditorPanel;
     }
 
     @Nullable
@@ -135,30 +150,30 @@ public class TemplateConfigurable extends BaseConfigurable {
         return null;
     }
 
-//    class ResetToDefaultAction extends AnAction {
-//
-//        ResetToDefaultAction() {
-//            super(StringResources.RESET_TO_DEFAULT_ACTION_TITLE, StringResources.RESET_TO_DEFAULT_ACTION_DESCRIPTION, AllIcons.Actions.Reset);
-//        }
-//
-//        @Override
-//        public void actionPerformed(AnActionEvent anActionEvent) {
-//            if (DialogsFactory.openResetTemplateDialog()) {
-//                templateSettings.removeTemplateForName(templateName);
-//                ApplicationManager.getApplication().runWriteAction(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        editor.getDocument().setText(templateSettings.provideTemplateForName(templateName));
-//                        setUnmodified();
-//                    }
-//                });
-//            }
-//        }
-//
-//        @Override
-//        public void update(AnActionEvent e) {
-//            super.update(e);
-//            e.getPresentation().setEnabled(templateSettings.isUsingCustomTemplateForName(templateName));
-//        }
-//    }
+    private class ResetToDefaultAction extends AnAction {
+
+        ResetToDefaultAction() {
+            super(StringResources.RESET_TO_DEFAULT_ACTION_TITLE, StringResources.RESET_TO_DEFAULT_ACTION_DESCRIPTION, AllIcons.Actions.Reset);
+        }
+
+        @Override
+        public void actionPerformed(AnActionEvent anActionEvent) {
+            if (DialogsFactory.openResetTemplateDialog()) {
+                mTemplateSettings.removeTemplateForName(mTemplateName);
+                ApplicationManager.getApplication().runWriteAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        mEditor.getDocument().setText(mTemplateSettings.provideTemplateForName(mTemplateName));
+                        setUnmodified();
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void update(AnActionEvent e) {
+            super.update(e);
+            e.getPresentation().setEnabled(mTemplateSettings.isUsingCustomTemplateForName(mTemplateName));
+        }
+    }
 }
--- PluginPractice/src/com/njnu/kai/plugin/preference/persistence/TemplateSettings.java
+++ PluginPractice/src/com/njnu/kai/plugin/preference/persistence/TemplateSettings.java
@@ -15,12 +15,12 @@ import java.util.Map;
 @State(
         name = "PluginPracticeTemplateSettings",
         storages = {
-                @Storage(file = StoragePathMacros.APP_CONFIG + "/plugin_practice_template_settings.xml")
+                @Storage(file = StoragePathMacros.WORKSPACE_FILE + "/plugin_practice_template_settings.xml")
         }
 )
 public class TemplateSettings implements PersistentStateComponent<TemplateSettings> {
 
-    private Map<String, String> templateValues = Maps.newHashMap();
+    private Map<String, String> mTemplateValues = Maps.newHashMap();
 
 //    private TemplatesProvider templatesProvider = new CodeGeneratorFactory.ResourceTemplateProvider();
 
@@ -29,11 +29,11 @@ public class TemplateSettings implements PersistentStateComponent<TemplateSettin
     }
 
     public Map<String, String> getTemplateValues() {
-        return templateValues;
+        return mTemplateValues;
     }
 
     public void setTemplateValues(Map<String, String> templateValues) {
-        this.templateValues = templateValues;
+        mTemplateValues = templateValues;
     }
 
     @Nullable
@@ -46,4 +46,47 @@ public class TemplateSettings implements PersistentStateComponent<TemplateSettin
     public void loadState(TemplateSettings templateSettings) {
         XmlSerializerUtil.copyBean(templateSettings, this);
     }
+
+    public String provideTemplateForName(String templateName) {
+        if (isUsingCustomTemplateForName(templateName)) {
+            return mTemplateValues.get(templateName);
+        }
+        if (templateName.contains("Activity")) {
+            return ACTIVITY_DEFAULT_TEMPLATE;
+        } else {
+            return String.format("Not setting default template for %s.", templateName);
+        }
+//        return templatesProvider.provideTemplateForName(templateName);
+    }
+
+    public void removeTemplateForName(String templateName) {
+        mTemplateValues.remove(templateName);
+    }
+
+    public boolean isUsingCustomTemplateForName(String templateName) {
+        return mTemplateValues.containsKey(templateName);
+    }
+
+    public void setTemplateForName(String templateName, String template) {
+        mTemplateValues.put(templateName, template);
+    }
+
+    private static final String ACTIVITY_DEFAULT_TEMPLATE = "import android.os.Bundle;\n" +
+            "import android.app.Activity;\n" +
+            "${IMPORTS}\n" +
+            "\n" +
+            "public class ${CLASS_NAME}Activity extends Activity ${INTERFACES} {\n" +
+            "\n" +
+            "    ${FIELDS}\n" +
+            "\n" +
+            "    @Override\n" +
+            "    protected void onCreate(Bundle savedInstanceState) {\n" +
+            "        super.onCreate(savedInstanceState);\n" +
+            "        setContentView(R.layout.${RESOURCE_NAME});\n" +
+            "\n" +
+            "        ${ASSIGNMENTS}\n" +
+            "    }\n" +
+            "\n" +
+            "    ${METHODS}\n" +
+            "}";
 }
