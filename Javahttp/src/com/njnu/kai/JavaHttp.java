5161775be66ac2634929a5aa2a8f5be23f6cb280
qianhk <qhk83@qq.com>
2016-09-03 18:48:16 +0800

    test parseByte

--- Javahttp/src/com/njnu/kai/JavaHttp.java
+++ Javahttp/src/com/njnu/kai/JavaHttp.java
@@ -7,13 +7,13 @@ import java.net.URLEncoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+//import org.apache.http.Header;
+//import org.apache.http.HttpEntity;
+//import org.apache.http.HttpResponse;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.client.methods.HttpGet;
+//import org.apache.http.impl.client.DefaultHttpClient;
+//import org.apache.http.util.EntityUtils;
 
 
 public class JavaHttp {
@@ -38,33 +38,33 @@ public class JavaHttp {
 	 }
 
 	public static void exeHttpClient(String urlAsString) throws IOException{
-	    HttpClient client = new DefaultHttpClient();
-	    try {
-	    	HttpGet httpGet = new HttpGet(urlAsString);
-	    	httpGet.setHeader("Referer", "http://baidu.com");
-	    	httpGet.setHeader("User-Agent", "kai kai browser 1983.12.24");
-	    	System.out.println("execting request: " + httpGet.getURI());
-	    	Header[] allHeader = httpGet.getAllHeaders();
-	    	for (Header h : allHeader) {
-	    		System.out.println(h);
-	    	}
-	    	HttpResponse response = client.execute(httpGet);
-	    	HttpEntity entity = response.getEntity();
-	    	System.out.println("------------------");
-	    	System.out.println(response.getStatusLine());
-	    	if (entity != null) {
-	    		String charset = EntityUtils.getContentCharSet(entity);
-	    		System.out.println("Response content length: " + entity.getContentLength() + " CharSet:" + charset);
-//	    		String ls_content = EntityUtils.toString(entity);
-	    		String ls_content = EntityUtils.toString(entity, ((charset == null) ? "UTF-8" : charset));
-//	    		String ls_content = EntityUtils.toString(entity, "UTF-8");
-	    		System.out.println("Response content: " + ((ls_content.length() > 1000) ? ls_content.substring(0, 1000) : ls_content));
-	    	}
-	    	System.out.println("--------------------");
-	    }
-	    finally {
-	    	client.getConnectionManager().shutdown();
-	    }
+//	    HttpClient client = new DefaultHttpClient();
+//	    try {
+//	    	HttpGet httpGet = new HttpGet(urlAsString);
+//	    	httpGet.setHeader("Referer", "http://baidu.com");
+//	    	httpGet.setHeader("User-Agent", "kai kai browser 1983.12.24");
+//	    	System.out.println("execting request: " + httpGet.getURI());
+//	    	Header[] allHeader = httpGet.getAllHeaders();
+//	    	for (Header h : allHeader) {
+//	    		System.out.println(h);
+//	    	}
+//	    	HttpResponse response = client.execute(httpGet);
+//	    	HttpEntity entity = response.getEntity();
+//	    	System.out.println("------------------");
+//	    	System.out.println(response.getStatusLine());
+//	    	if (entity != null) {
+//	    		String charset = EntityUtils.getContentCharSet(entity);
+//	    		System.out.println("Response content length: " + entity.getContentLength() + " CharSet:" + charset);
+////	    		String ls_content = EntityUtils.toString(entity);
+//	    		String ls_content = EntityUtils.toString(entity, ((charset == null) ? "UTF-8" : charset));
+////	    		String ls_content = EntityUtils.toString(entity, "UTF-8");
+//	    		System.out.println("Response content: " + ((ls_content.length() > 1000) ? ls_content.substring(0, 1000) : ls_content));
+//	    	}
+//	    	System.out.println("--------------------");
+//	    }
+//	    finally {
+//	    	client.getConnectionManager().shutdown();
+//	    }
 	}
 
 	public static void main(String[] args) throws IOException {
--- Javahttp/src/com/njnu/kai/Test.java
+++ Javahttp/src/com/njnu/kai/Test.java
@@ -7,19 +7,21 @@ package com.njnu.kai;
  */
 
 
-
-
 public class Test {
-	public static String readableByte(long bytes){
-		int unit=1024;
-		if(bytes<unit)return bytes+"B";
-		int exp=(int)(Math.log(bytes)/Math.log(unit));
-		char pre="KMGTPE".charAt(exp-1);
-		return String.format("%.1f%cB",bytes/Math.pow(unit,exp),pre);
-	}
+    public static String readableByte(long bytes) {
+        int unit = 1024;
+        if (bytes < unit) return bytes + "B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        char pre = "KMGTPE".charAt(exp - 1);
+        return String.format("%.1f%cB", bytes / Math.pow(unit, exp), pre);
+    }
 
-	public static void main(String[] args)
-	{
-
-	}
+    public static void main(String[] args) {
+        System.err.println(Integer.parseInt("ca", 16));
+        System.err.println(Integer.parseInt("d5", 16));
+        short shortD5 = Short.parseShort("d5", 16);
+        System.err.println(shortD5);
+        System.err.println((byte)shortD5);
+//        System.err.println(Integer.parseInt("0xca"));
+    }
 }
