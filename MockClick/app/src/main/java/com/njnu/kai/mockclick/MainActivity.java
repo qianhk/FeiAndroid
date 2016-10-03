9e5ea40d5ce1ba35f9f4f0273fbf33de440c48f6
qianhk <qhk83@qq.com>
2016-10-03 13:28:37 +0800

    show click times

--- MockClick/app/src/main/java/com/njnu/kai/mockclick/MainActivity.java
+++ MockClick/app/src/main/java/com/njnu/kai/mockclick/MainActivity.java
@@ -1,8 +1,9 @@
 package com.njnu.kai.mockclick;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,29 +13,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar mToolbar;
+    private View mLayoutContent;
+    private TextView mTvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mLayoutContent = findViewById(R.id.content_main);
+        mTvResult = (TextView) mLayoutContent.findViewById(R.id.tv_result);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
@@ -97,5 +108,55 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private StringBuilder mBuilder = new StringBuilder();
+    private int mEventMoveCount;
+    private int mEventOtherCount;
+
+    private int mClickCount;
+
+    private long mLastElapsedTime;
+
+    private long mFirstElapsedTime;
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int actionMasked = event.getActionMasked();
+        int rawX = (int) event.getRawX();
+        int rawY = (int) event.getRawY();
+        switch (actionMasked) {
+            case MotionEvent.ACTION_DOWN:
+                if (SystemClock.elapsedRealtime() - mLastElapsedTime > 600) {
+                    mClickCount = 0;
+                    mFirstElapsedTime = SystemClock.elapsedRealtime();
+                }
+                mBuilder.setLength(0);
+                mEventMoveCount = 0;
+                mEventOtherCount = 0;
+                mBuilder.append(String.format(Locale.getDefault(), "down:[%d,%d]", rawX, rawY));
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                ++mEventMoveCount;
+                break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                ++mClickCount;
+                mLastElapsedTime = SystemClock.elapsedRealtime();
+                mBuilder.append(String.format(Locale.getDefault(), " move:%d other:%d", mEventMoveCount, mEventOtherCount));
+                mBuilder.append(String.format(Locale.getDefault()
+                        , " %s:[%d,%d] clicked:%d elapse:%d"
+                        , actionMasked == MotionEvent.ACTION_UP ? "up" : "cancel"
+                        , rawX, rawY, mClickCount, mLastElapsedTime - mFirstElapsedTime));
+                mTvResult.setText(mBuilder.toString());
+                break;
+
+            default:
+                ++mEventOtherCount;
+                break;
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
