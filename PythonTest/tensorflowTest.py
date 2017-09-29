a376a85d4b8210f17c2c2cfab2d5cab5379da6a1
qianhk <qhk83@qq.com>
2017-09-29 22:02:54 +0800

    test tensorflow

---
+++ PythonTest/tensorflowTest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+sess.run(hello)
+
+a = tf.constant(10)
+b = tf.constant(32)
+sess.run(a + b)
+sess.close()
