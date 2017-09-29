94286a2301b10c2425f0a3d7da0bcf1089164286
qianhk <qhk83@qq.com>
2017-09-30 00:06:12 +0800

    need print to see result

--- PythonTest/tensorflowTest.py
+++ PythonTest/tensorflowTest.py
@@ -2,11 +2,12 @@
 # coding=utf-8
 
 import tensorflow as tf
+
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
-sess.run(hello)
+print(sess.run(hello))
 
 a = tf.constant(10)
 b = tf.constant(32)
-sess.run(a + b)
+print(sess.run(a + b))
 sess.close()
